c96febd546fd794f405972a35f8390874bf38de8
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-11-19 18:56:30 +0200

    created the page

---
+++ clock.js
@@ -0,0 +1,29 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getUTCHours();
+  var m = today.getUTCMinutes();
+  var s = today.getUTCSeconds();
+
+  if (h > 24) {
+    h = h - 24;
+  }
+  if (h < 0) {
+    h = h + 24;
+  }
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  };
+  return i;
+}
+
+startTime()
